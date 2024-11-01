bc21cea8c54b725ea8ed4f003c60e516975162b5
Marco Meyer-Conde　　　  メイヤーマコト <37673095+meiyasan@users.noreply.github.com>
2024-11-01 09:51:24 +0900

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,9 @@
 #! /usr/bin/bash
 
-#mkdir build
-#cd build
+mkdir build
+cd build
 
-sh CI/build.sh cmake tarball GNU nodebug
-#cmake .. -DCMAKE_INSTALL_PREFIX=${PWD}/../cern/new
+cmake .. -DCMAKE_INSTALL_PREFIX=${PWD}/../cern/new
 
-#make
-#make install
+make
+make install
