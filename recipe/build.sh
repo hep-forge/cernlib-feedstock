06714921528b5f660b805742059419aa06990eb2
Marco Meyer-Conde <marco@tcu.ac.jp>
2024-12-19 17:47:03 +0900

    add no pie patch

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,15 +1,16 @@
 #! /usr/bin/bash
 
-kmkdir build-scripts
+mkdir -p build-scripts
 cd build-scripts
 cmake $RECIPE_DIR/scripts
 
-mkdir ../build
+mkdir -p ../build
 cd ../build
 
 cmake .. -DCMAKE_INSTALL_PREFIX=${PREFIX} \
--DCERNLIB_BUILD_SHARED=OFF \ # @TODO: issue with openmotif
 -DCERNLIB_POSITION_INDEPENDENT_CODE=ON
+# @TODO: openmotif compatibility issue
+# -DCERNLIB_BUILD_SHARED=OFF \
 
 make -j$(nproc)
 make install
