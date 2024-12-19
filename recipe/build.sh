0dd904b09b06bc2b7624f1735d5c8fe562e5d2b1
Marco Meyer-Conde <marco@tcu.ac.jp>
2024-12-19 17:17:40 +0900

    add hbook test

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,15 @@
 #! /usr/bin/bash
 
-mkdir build-scripts
+kmkdir build-scripts
 cd build-scripts
 cmake $RECIPE_DIR/scripts
 
 mkdir ../build
 cd ../build
 
-cmake .. -DCMAKE_INSTALL_PREFIX=${PREFIX}
+cmake .. -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+-DCERNLIB_BUILD_SHARED=OFF \ # @TODO: issue with openmotif
+-DCERNLIB_POSITION_INDEPENDENT_CODE=ON
+
 make -j$(nproc)
 make install
